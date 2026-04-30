62586015c1d4456bfa412bc22a81014c35cfc109
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-04-29 22:14:22 -0600

    routing and adding public folders

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 // Import express using ESM syntax
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// Create an instance of an express application
+
+// app = express() creates an instance for the application // below important variable section
 const app = express();
+const PORT = process.env.PORT || 3000; // set the port from .env file or default to 3000
+const name = process.env.NAME || "Guest";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const name = process.env.NAME;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')))
 
-// Define a route handler for the root URL ('/')
+// Define a route handler for the root URL ('/') HERE WE DECLARED ROUTES
 app.get('/', (req,res)=> {
-    res.send(`Hellow guys! ${name}, welcome to my server!`);
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+})
 
-//  Define the port number the server will listen on
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+})
+
 
 // Start the server and listen on the defined port
 app.listen(PORT, () =>{
