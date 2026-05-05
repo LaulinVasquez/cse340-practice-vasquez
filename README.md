f89ada809ccb7cbccdcb5079d304b60784e65e5e
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-05-05 17:58:34 -0600

    error handlers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cse340-practice-vasquez
 Practice project for CSE 340
+
+
+Start the server using - pnpm run 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import { console } from 'inspector/promises';
+
 
 
 // app = express() creates an instance for the application 
@@ -38,6 +38,45 @@ app.get('/products', (req, res) => {
     const title = "Products";
     res.render('products', { title });
 });
+
+app.get('/test-error', (req, res, next) => {
+    const err = new Error('This is a test error');
+    err.status = 500;
+    next(err);
+});
+
+// Global error handling middleware for 404 and other errors
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+// Global error handling for 404s and 500s
+app.use((err, req, res, next) => {
+    // Prevent infinite loop if headers are already sent
+    if (res.headerSent || res.finished) {
+        return next(err);
+    }
+    // Determine the states code and template
+    const status = err.status || 500;
+    const message = status === 404 ? "404": "500";
+    const context = {
+        title: status === 404 ? "Page Not Found": "Internal Server Error",
+        error: NODE_ENV === "production" ? "An error occured" : err.message, // Show error details only in development
+        stack: NODE_ENV === "production" ? null : err.stack,
+        NODE_ENV // Our Websocket check needs this and its convenient to pass along
+    }
+    // Render the appropriate error template with fallback
+    try{
+        res.status(status).render(`errors/${message}`, context);
+    }catch (renderErr) {
+        // If rendering fails, send a simple text response
+        if (!res.headerSent){
+            res.status(status).send(`<h1>Error ${status}</h1><p>An error occurred.</p>`);
+        }
+    }
+    
+})
 
 // Start the server and listen on the defined port
 app.listen(PORT, () =>{
